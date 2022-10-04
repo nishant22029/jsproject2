e8bbd283da521dd58a3e528b3cb9a6f740467be9
nishant kumar <nishantumu@gmail.com>
2022-10-04 16:17:29 +0530

    project2 js

---
+++ index.js
@@ -0,0 +1,23 @@
+let side1=5;
+let side2=6;
+let side3=7;
+let area=side1+side2+side3;
+console.log("area of triangle="+area)
+let file1="hello.java"
+let file2="Program.php"
+let file3="index.html"
+console.log("file1 extension="+file1.split('.').pop())
+console.log("file2 extension="+file2.split('.').pop())
+console.log("file3 extension="+file3.split('.').pop())
+
+let num1=parseInt(prompt("enter number 1"))
+let num2=parseInt(prompt("enter number 2"))
+let sum;
+if(num1==num2){
+    sum=3*(num1+num2)
+    console.log("sum ="+sum)
+}
+   
+else
+    {sum=num1+num2;
+        console.log("sum of two number="+sum)}
